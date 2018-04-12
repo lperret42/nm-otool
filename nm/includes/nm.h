fa68bdc1a0ea9f021e1a86472b65d621ae580924
Lucas PERRET <lperret@e1r5p3.42.fr>
2018-04-12 15:07:16 +0200

    beginning handle .a files

--- nm/includes/nm.h
+++ nm/includes/nm.h
@@ -2,8 +2,10 @@
 # define NM_H
 
 # include <sys/mman.h>
+# include <ar.h>
 # include <mach-o/loader.h>
 # include <mach-o/nlist.h>
+# include <mach-o/ranlib.h>
 # include <fcntl.h>
 # include <sys/stat.h>
 # include <stdlib.h>
@@ -20,6 +22,8 @@ typedef enum		e_error
 }					t_error;
 
 int		handle_error(t_error error);
+void	handle_64(char *ptr);
+void	handle_ar(char *ptr);
 void	nm(char *ptr);
 
 #endif
