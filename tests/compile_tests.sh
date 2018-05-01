fac5b35d8872928fe3d5ca738e9e08335e1fe102
Lucas PERRET <lperret@e1r8p1.42.fr>
2018-05-01 15:28:39 +0200

    cleaning

--- tests/compile_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc my_test.c -o my_test
-gcc test_facile.c -o test_facile
-gcc test_moins_facile.c -o test_moins_facile
--- tests/my_test.c
+++
@@ -1,9 +0,0 @@
-static int s = 3;
-
-int main()
-{
-	static int	a = 3;
-	int			b = 253;
-
-	return 0;
-}
